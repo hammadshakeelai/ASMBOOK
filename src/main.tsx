ce26ac507d313f9cbaf4b4be0e39aa056aef2fb1
hammadai <hammadshakeel61@gmail.com>
2026-09-04 21:07:21 +0500

    Fix case-sensitive import of App.tsx on Linux (Pages build)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { render } from 'preact';
-import { App } from './ui/app.js';
+import { App } from './ui/App.js';
 import './ui/style.css';
 
 render(<App />, document.getElementById('app')!);
